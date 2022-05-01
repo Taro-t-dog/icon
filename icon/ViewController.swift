c0e52b80688aab6b052a95b20876d0b52da659a9
Taro Segawa(瀬川太朗） <tdog7305@icloud.com>
2022-05-01 20:54:09 +0900

    Initial Commit

---
+++ icon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  icon
+//
+//  Created by 瀬川太朗 on 2022/05/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
